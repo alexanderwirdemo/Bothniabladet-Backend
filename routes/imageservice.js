65ac760c9a51a30f16969c41b14f409f4c9dc798
Erdymus <hamfer-0@student.ltu.se>
2022-04-14 21:12:01 +0200

    GET-anrop för title, category, subcategory, news event

--- routes/imageservice.js
+++ routes/imageservice.js
@@ -2,7 +2,6 @@ module.exports = function(app, Image){
 
 
     // GET-anrop för att få alla bilder
-    // Kommentar
     app.get("/images", function(req, res) {
         console.log('Finding all images....');
         var allImages = [];
@@ -22,5 +21,92 @@ module.exports = function(app, Image){
             }); 
         });
     });
-    
-    }
+
+    // GET-anrop för att få bilder baserat på titel
+    app.get("/title", function(req, res) {
+        console.log('Finding images based on title....');
+        var foundImagesTitle = [];
+        var query = { "Administrative_data.title": "Växjö Lakers målvakt" };
+        Image.find(query, function(err, result){
+            if(err){
+                console.log(err);
+                res.send(err);
+            }
+            for(let index=0; index<result.length; index++){
+                console.dir(result[index]._doc);
+                foundImagesTitle.push(result[index]._doc); 
+            }
+            console.dir(foundImagesTitle);
+            
+            return res.json({
+                foundImagesTitle
+            }); 
+        });
+    });
+
+    // GET-anrop för att få bilder baserat på kategori
+    app.get("/category", function(req, res) {
+        console.log('Finding images based on category....');
+        var foundImagesCategory = [];
+        var query = { "category": "Sport" };
+        Image.find(query, function(err, result){
+            if(err){
+                console.log(err);
+                res.send(err);
+            }
+            for(let index=0; index<result.length; index++){
+                console.dir(result[index]._doc);
+                foundImagesCategory.push(result[index]._doc); 
+            }
+            console.dir(foundImagesCategory);
+            
+            return res.json({
+                foundImagesCategory
+            }); 
+        });
+    });
+
+    // GET-anrop för att få bilder baserat på underkategori
+    app.get("/subcategory", function(req, res) {
+        console.log('Finding images based on subcategory....');
+        var foundImagesSubcategory = [];
+        var query = { "subcategory": "Ishockey" };
+        Image.find(query, function(err, result){
+            if(err){
+                console.log(err);
+                res.send(err);
+            }
+            for(let index=0; index<result.length; index++){
+                console.dir(result[index]._doc);
+                foundImagesSubcategory.push(result[index]._doc); 
+            }
+            console.dir(foundImagesSubcategory);
+            
+            return res.json({
+                foundImagesSubcategory
+            }); 
+        });
+    });    
+
+    // GET-anrop för att få bilder baserat på nyhetshändelse
+    app.get("/event", function(req, res) {
+        console.log('Finding images based on news event....');
+        var allImagesEvent = [];
+        var query = { "news_event": "Regerande världsmästare tog prestigefull förstaplacering i Saudi Arabian grand prix" };
+        Image.find(query, function(err, result){
+            if(err){
+                 console.log(err);
+                res.send(err);
+            }
+            for(let index=0; index<result.length; index++){
+                console.dir(result[index]._doc);
+                allImagesEvent.push(result[index]._doc); 
+            }
+            console.dir(allImagesEvent);
+                
+            return res.json({
+                allImagesEvent
+            }); 
+        });
+    });    
+}
